e52b6caa363b7886868788443f1c39179937b4ba
hncgc1990 <hncgc1990@163.com>
2017-09-13 11:33:42 +0800

    添加ConstraintLayout的demo

---
+++ MyApplication4/app/src/main/java/com/hncgc1990/myrefreshview/constrainlayout/ConstrainLayoutActivity.java
@@ -0,0 +1,15 @@
+package com.hncgc1990.myrefreshview.constrainlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hncgc1990.myrefreshview.R;
+
+public class ConstrainLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constrain_demo);
+    }
+}
